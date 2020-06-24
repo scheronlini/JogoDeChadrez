4409bfc1627f737da6972f2d04eabc425bbc9813
scheronlini <scheronlini@gmail.com>
2020-06-24 19:15:08 -0300

    Criada a classe Posicao

--- src/Aplicacao/programa.java
+++ src/Aplicacao/programa.java
@@ -1,10 +1,13 @@
 package Aplicacao;
 
+import Tabuleiro.Posicao;
+
 public class programa {
 	
 	
 	public static void main(String[] args) {
-		System.out.println("oi");
+		Posicao pos = new Posicao (3,5);
+		System.out.println(pos);
 			}
 
 }
